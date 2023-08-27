765b019c0442708ca787afbd1da49849569cd402
Test <bloodreighn965@gmail.com>
2023-08-27 23:17:26 +0300

    Initialcommit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int price =13 676
+        System.out.println("Hello world!");
+
+        int bonus=price/20
+                System.out.println("Бонус:" +bonus);
+    }
+}
